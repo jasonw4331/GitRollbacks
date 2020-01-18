4f42217561ada2b57e0a205dfe065b91b6b95a02
jasonwynn10 <jasonwynn10@me.com>
2020-01-17 21:40:06 -0500

    Fixed level rollback error

--- src/jasonwynn10/GitRollbacks/RollbackLevelTask.php
+++ src/jasonwynn10/GitRollbacks/RollbackLevelTask.php
@@ -2,21 +2,24 @@
 declare(strict_types=1);
 namespace jasonwynn10\GitRollbacks;
 
+use pocketmine\level\Level;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 
 class RollbackLevelTask extends AsyncTask {
 
 	/** @var string */
-	private $commitHash, $gitFolder, $levelName;
-	/** @var bool */
-	private $force;
+	private $commitHash, $gitFolder, $levelName, $levelPath;
 
 	public function __construct(string $gitFolder, string $levelName, string $commitHash, bool $force) {
 		$this->gitFolder = $gitFolder;
 		$this->commitHash = $commitHash;
 		$this->levelName = $levelName;
-		$this->force = $force;
+		$level = Server::getInstance()->getLevelByName($levelName);
+		if($level instanceof Level) {
+			$this->levelPath = $level->getProvider()->getPath();
+			Server::getInstance()->unloadLevel($level, $force); // force unload for rollback of default world
+		}
 	}
 
 	/**
@@ -27,20 +30,7 @@ class RollbackLevelTask extends AsyncTask {
 	 */
 	public function onRun() {
 		$git = new GitRepository($this->gitFolder);
-		//$git->checkout($this->commitHash);
 		$git->reset($this->commitHash);
-		$count = 1;
-		foreach($git->getBranches() ?? [] as $branch) {
-			if($branch === "master")
-				continue;
-			$count = substr($branch, 8);
-			$count += (int)$count;
-		}
-		$git->createBranch("Rollback".$count, true);
-		$level = Server::getInstance()->getLevelByName($this->levelName);
-		$return = Server::getInstance()->unloadLevel($level, $this->force); // force unload for rollback of default world
-		if(!$return)
-			return;
-		Main::recursiveCopyAddGit($this->gitFolder, $level->getProvider()->getPath());
+		Main::recursiveCopyAddGit($this->gitFolder, $this->levelPath);
 	}
 }
